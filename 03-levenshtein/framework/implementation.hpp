6415ca0b0914adde4ab7a975b71b12b926adc8d5
Hruby Krystof Bc <hrubykr@parlab.ms.mff.cuni.cz>
2022-04-02 20:41:07 +0200

    with comments

--- 03-levenshtein/framework/implementation.hpp
+++ 03-levenshtein/framework/implementation.hpp
@@ -5,22 +5,21 @@
 #include <algorithm>
 #include <iostream>
 #include <tuple>
+#include <cassert>
 
 template<typename C = char, typename DIST = std::size_t, bool DEBUG = false>
 class EditDistance : public IEditDistance<C, DIST, DEBUG> {
 public:
 
-	int qwe = 42;
 	/*
 	 * \brief Perform the initialization of the functor (e.g., allocate memory buffers).
 	 * \param len1, len2 Lengths of first and second string respectively.
 	 */
 	virtual void init(DIST len1, DIST len2) {
-		_currentStripe.resize((std::size_t)std::min<DIST>(len1, len2) + 2);
-		_previousStripe.resize((std::size_t)std::min<DIST>(len1, len2) + 2);
-		_previousPreviousStripe.resize((std::size_t)std::min<DIST>(len1, len2) + 2);
+		_currentStripe.resize((std::size_t)std::max<DIST>(len1, len2) + 2);
+		_previousStripe.resize((std::size_t)std::max<DIST>(len1, len2) + 2);
+		_previousPreviousStripe.resize((std::size_t)std::max<DIST>(len1, len2) + 2);
 	}
-
 
 	/*
 	 * \brief Compute the distance between two strings.
@@ -30,8 +29,10 @@ public:
 	virtual DIST compute(const std::vector<C> &str1, const std::vector<C> &str2) {
 		if (str1.size() <= str2.size()) {
 			return lev(str1, str2);
+			// return lev3T(str1, str2);
 		} else {
 			return lev(str2, str1);
+			// return lev3T(str2, str1);
 		}
 	}
 
@@ -54,26 +55,23 @@ private:
 			// std::cout << std::endl;
 			std::tie(currentStripeStart, currentStripeEnd) = getCurrentStripeRange(a.size(), b.size(), i, currentStripeStart, currentStripeEnd);
 			bool lastTriangle = i >= b.size();
-			if (!lastTriangle) {
-				_currentStripe[0] = i + 2;
+			_currentStripe[0] = i + 2;
+			if (currentStripeEnd < _currentStripe.size()) {
 				_currentStripe[currentStripeEnd] = _currentStripe[0];
 			}
 
-			std::size_t xx = 0;
 			// std::cout << "STRIPE " << i << " from " << currentStripeStart << " to " << currentStripeEnd << std::endl;
-			// #pragma omp parallel for 
+			#pragma omp parallel for
 			for(std::size_t currentStripeIndex = currentStripeStart; currentStripeIndex < currentStripeEnd; ++currentStripeIndex) {
 				C aChar = 0;
-				++xx;
 				if (lastTriangle) {
-					aChar = a[a.size() - (xx)];
+					aChar = a[a.size() - (currentStripeIndex - currentStripeStart + 1)];
 				} else {
 					aChar = a[currentStripeEnd - currentStripeIndex - 1];
 				}
 				// std::cout << "Comparing "
 				//  << (char)aChar << " and "
 				//  << (char)b[currentStripeIndex - 1];
-
 
 				DIST substitutionCost = (b[currentStripeIndex - 1] == aChar) ? 0 : 1;
 				_currentStripe[currentStripeIndex] = std::min<DIST>({
@@ -107,7 +105,6 @@ private:
 			std::swap(_previousStripe, _previousPreviousStripe);
 		}
 		return _previousStripe[currentStripeStart];
-
 	}
 
 	std::tuple<std::size_t, std::size_t> getCurrentStripeRange(std::size_t aLen, std::size_t bLen, std::size_t i, std::size_t currentStripeStart, std::size_t currentStripeEnd) {
@@ -121,6 +118,111 @@ private:
 
 		return {++currentStripeStart, currentStripeEnd};
 	}
+
+DIST lev3T(const std::vector<C>& a, const std::vector<C>& b) { // a.len <= b.len
+		std::size_t currentStripeStart = 1;
+		std::size_t currentStripeEnd = 1;
+
+		_previousStripe[0] = 1;
+		_previousStripe[1] = 1;
+
+		std::size_t iterCount = a.size() + b.size() - 1;
+		std::size_t i = 0;
+		for(; i < 1000; ++i) {
+
+			std::tie(currentStripeStart, currentStripeEnd) = getCurrentStripeRange(a.size(), b.size(), i, currentStripeStart, currentStripeEnd);
+			bool lastTriangle = i >= b.size();
+			if (!lastTriangle) {
+				_currentStripe[0] = i + 2;
+				_currentStripe[currentStripeEnd] = _currentStripe[0];
+			}
+
+			for(std::size_t currentStripeIndex = currentStripeStart; currentStripeIndex < currentStripeEnd; ++currentStripeIndex) {
+				C aChar = 0;
+				if (lastTriangle) {
+					aChar = a[a.size() - (currentStripeIndex - currentStripeStart + 1)];
+				} else {
+					aChar = a[currentStripeEnd - currentStripeIndex - 1];
+				}
+
+				DIST substitutionCost = (b[currentStripeIndex - 1] == aChar) ? 0 : 1;
+				_currentStripe[currentStripeIndex] = std::min<DIST>({
+					_previousStripe[currentStripeIndex] + (DIST)1, 
+					_previousStripe[currentStripeIndex - 1] + (DIST)1,  
+					_previousPreviousStripe[currentStripeIndex - 1] + substitutionCost 
+				});
+			}
+
+			std::swap(_currentStripe, _previousPreviousStripe);
+			std::swap(_previousStripe, _previousPreviousStripe);
+		}
+
+		for(; i < iterCount - 1000; ++i) {
+
+			std::tie(currentStripeStart, currentStripeEnd) = getCurrentStripeRange(a.size(), b.size(), i, currentStripeStart, currentStripeEnd);
+			bool lastTriangle = i >= b.size();
+			if (!lastTriangle) {
+				_currentStripe[0] = i + 2;
+				_currentStripe[currentStripeEnd] = _currentStripe[0];
+			}
+
+			// #pragma omp parallel for 
+			std::size_t blockSize = 256;
+			std::size_t blockCount = (currentStripeEnd - currentStripeStart) / blockSize;
+			for (std::size_t block = currentStripeStart; block < blockCount; block += blockSize) {
+
+				for(std::size_t currentStripeIndex = block; currentStripeIndex < currentStripeEnd; ++currentStripeIndex) {
+					C aChar = 0;
+					if (lastTriangle) {
+						aChar = a[a.size() - (currentStripeIndex - currentStripeStart + 1)];
+					} else {
+						aChar = a[currentStripeEnd - currentStripeIndex - 1];
+					}
+
+					DIST substitutionCost = (b[currentStripeIndex - 1] == aChar) ? 0 : 1;
+					_currentStripe[currentStripeIndex] = std::min<DIST>({
+						_previousStripe[currentStripeIndex] + (DIST)1, 
+						_previousStripe[currentStripeIndex - 1] + (DIST)1,  
+						_previousPreviousStripe[currentStripeIndex - 1] + substitutionCost 
+					});
+				}
+			}
+
+			std::swap(_currentStripe, _previousPreviousStripe);
+			std::swap(_previousStripe, _previousPreviousStripe);
+		}
+
+		for(; i < iterCount; ++i) {
+
+			std::tie(currentStripeStart, currentStripeEnd) = getCurrentStripeRange(a.size(), b.size(), i, currentStripeStart, currentStripeEnd);
+			bool lastTriangle = i >= b.size();
+			if (!lastTriangle) {
+				_currentStripe[0] = i + 2;
+				_currentStripe[currentStripeEnd] = _currentStripe[0];
+			}
+
+			for(std::size_t currentStripeIndex = currentStripeStart; currentStripeIndex < currentStripeEnd; ++currentStripeIndex) {
+				C aChar = 0;
+				if (lastTriangle) {
+					aChar = a[a.size() - (currentStripeIndex - currentStripeStart + 1)];
+				} else {
+					aChar = a[currentStripeEnd - currentStripeIndex - 1];
+				}
+
+				DIST substitutionCost = (b[currentStripeIndex - 1] == aChar) ? 0 : 1;
+				_currentStripe[currentStripeIndex] = std::min<DIST>({
+					_previousStripe[currentStripeIndex] + (DIST)1, 
+					_previousStripe[currentStripeIndex - 1] + (DIST)1,  
+					_previousPreviousStripe[currentStripeIndex - 1] + substitutionCost 
+				});
+			}
+
+			std::swap(_currentStripe, _previousPreviousStripe);
+			std::swap(_previousStripe, _previousPreviousStripe);
+		}
+		return _previousStripe[currentStripeStart];
+	}
+
 };
 
 
